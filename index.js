aded979e20747608736a04802f60e3eed3e251f2
alexejb <stillefront@users.noreply.github.com>
2017-11-27 13:52:29 +0100

    refactored sent messages into JSON objects and implemented differentiation between users through automatically fetched user id

--- index.js
+++ index.js
@@ -6,21 +6,36 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+
+
+
+
 io.on('connection', function(socket){
-	console.log('a user connected');
+	console.log(socket.id + ' connected'); //for debuging in console
+
+	socket.send(JSON.stringify(
+		{type:'serverMessage',
+		message: 'Wilkommen im Chat!'
+		}));
+
 	
 	socket.on('disconnect', function(){
 	console.log('user disconnected');	
 	});
 
-	socket.on('chat message', function(msg){
-
-	io.emit('chat message', msg);	
-
-	console.log('message: ' + msg);	
+		socket.on('message', function(message){
+		message = JSON.parse(message);
+		if(message.type == "userMessage"){
+			socket.broadcast.send(JSON.stringify(message));
+			message.type = "myMessage";
+			socket.send(JSON.stringify(message));
+			}
+		console.log(socket.id + ' wrote: ' + message.message); // debug incoming messages	
+		});
+	
 	});
 
-});
+
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
