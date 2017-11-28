dc91dd215678713ea6ea05179c19cb19f701091f
alexejb <stillefront@users.noreply.github.com>
2017-11-28 15:06:36 +0100

    small dialogue to get the username, no more socket.ids

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+var express   =  require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var people = {};
+
+
+
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
+	console.log("GET index.html");
 });
+
+app.use('/stylesheets', express.static(__dirname + '/stylesheets')); // let express use static directories on a GET request: https://stackoverflow.com/questions/5924072/express-js-cant-get-my-static-files-why#5924732 
 
 
 
@@ -12,11 +20,6 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket){
 	console.log(socket.id + ' connected'); //for debuging in console
-
-	socket.send(JSON.stringify(
-		{type:'serverMessage',
-		message: 'Wilkommen im Chat!'
-		}));
 
 	
 	socket.on('disconnect', function(){
@@ -26,11 +29,19 @@ io.on('connection', function(socket){
 		socket.on('message', function(message){
 		message = JSON.parse(message);
 		if(message.type == "userMessage"){
-			socket.broadcast.send(JSON.stringify(message));
+			socket.broadcast.send(people[socket.id], JSON.stringify(message));
 			message.type = "myMessage";
-			socket.send(JSON.stringify(message));
+			socket.send(people[socket.id], JSON.stringify(message));
+
 			}
-		console.log(socket.id + ' wrote: ' + message.message); // debug incoming messages	
+		console.log(people[socket.id] + ' wrote: ' + message.message); // debug incoming messages	
+		});
+
+		socket.on("set_name", function(data){
+			people[socket.id] = data;
+			console.log(people[socket.id]);
+
+			socket.emit('name_set', people[socket.id]);			
 		});
 	
 	});
